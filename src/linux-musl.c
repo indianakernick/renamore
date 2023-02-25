314ede2a56a6a7033aa9c31a935c9e77a91cfb39
Indiana Kernick <indiana.kernick@gmail.com>
2023-02-25 15:23:21 +1030

    Make the musl implementation of renameat2 a weak symbol

--- src/linux-musl.c
+++ src/linux-musl.c
@@ -2,6 +2,7 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
+__attribute__((weak, visibility("default")))
 int renameat2(int oldfd, const char *old, int newfd, const char *new, unsigned int flags) {
     return syscall(SYS_renameat2, oldfd, old, newfd, new, flags);
 }
